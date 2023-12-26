75cf6988458ad4b195c118bb583b5fb43f6c3398
Tanish . <tanishmore54487@gmail.com>
2023-12-26 12:04:40 +0530

    Commit 1

---
+++ final/controllers/main.js
@@ -0,0 +1,38 @@
+
+
+const jwt = require('jsonwebtoken')
+const { BadRequestError } = require('../errors')
+
+const login = async (req, res) => {
+  const { username, password } = req.body
+
+
+  if (!username || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+
+  //just for demo 
+  const id = new Date().getDate()
+
+  // small payload for better user experience
+  // in production there are long jwtsecret but i do not know how to implement that yet..
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+
+  res.status(200).json({ msg: 'user created', token })
+}
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100)
+
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, which is your lucky number is ${luckyNumber}`,
+  })
+}
+
+module.exports = {
+  login,
+  dashboard,
+}
